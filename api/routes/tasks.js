9a590da501a4929051bf41e411e8fb20893b97e4
Thuy Trang Pham <147356022+anniehayho@users.noreply.github.com>
2024-05-08 00:20:25 +0700

    install firebase

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+import firebase_db from '../../firebase/firebaseConfig';
 
 //Hanle incoming GET requests to /tasks
 router.get('/', (req, res, next) => {
